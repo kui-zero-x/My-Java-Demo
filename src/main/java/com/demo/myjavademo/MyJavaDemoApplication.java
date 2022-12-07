a09c961af06f56bd951f31fb124870d4973fe451
lcg7586943 <lcg7586943@163.com>
2022-12-07 10:30:54 +0800

    first commit

---
+++ src/main/java/com/demo/myjavademo/MyJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.myjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJavaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/myjavademo/MyJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.myjavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
