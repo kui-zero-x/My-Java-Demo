01a2fc2cf3cf1c7005d77f90bc559db32403b832
lcg7586943 <lcg7586943@163.com>
2023-02-15 10:20:07 +0800

    test

--- src/main/java/com/demo/myjavademo/demo/stream/StreamTest.java
+++ src/main/java/com/demo/myjavademo/demo/stream/StreamTest.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 public class StreamTest {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(new Integer[]{1,1,1,4,5,6}));
+        List<Integer> list = new ArrayList<>(Arrays.asList(new Integer[]{1, 1, 1, 4, 5, 6}));
         System.out.println(Stream.concat(list.stream().filter(t -> t > 2).distinct().map(t -> ++t), list.stream().filter(t -> t == 1)).collect(Collectors.toList()));
     }
 
@@ -20,7 +20,7 @@ public class StreamTest {
      * 在执行返回 Stream 的方法时，并不立刻执行，而是等返回一个非 Stream 的方法后才执行
      */
     @Test
-    public void laziness(){
+    public void laziness() {
         List<String> strings = Arrays.asList("abc", "def", "gkh", "abc");
         Stream<Integer> stream = strings.stream().filter(new Predicate() {
             @Override
@@ -32,5 +32,11 @@ public class StreamTest {
 
         System.out.println("count 执行");
         stream.count();
+    }
+
+    @Test
+    public void join_test() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(new Integer[]{1, 1, 1, 4, 5, 6}));
+        System.out.println(list.stream().map(String::valueOf).collect(Collectors.joining("|", "[", "]")));
     }
 }
