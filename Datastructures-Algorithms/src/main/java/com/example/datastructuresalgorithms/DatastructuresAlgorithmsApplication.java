512a455350599ce392743aeed58b5f63023b53a3
lcg7586943 <lcg7586943@163.com>
2023-02-20 14:44:48 +0800

    test

---
+++ Datastructures-Algorithms/src/main/java/com/example/datastructuresalgorithms/DatastructuresAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastructuresalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructuresAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatastructuresAlgorithmsApplication.class, args);
+    }
+
+}
