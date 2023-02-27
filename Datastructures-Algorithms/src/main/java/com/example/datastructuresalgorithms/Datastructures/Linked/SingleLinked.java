34b927ffebbe0e4663a96646c8dbeb8bcde3df1c
kui-zero <lcg7586943@163.com>
2023-02-27 22:13:46 +0800

    DA

--- Datastructures-Algorithms/src/main/java/com/example/datastructuresalgorithms/Datastructures/Linked/SingleLinked.java
+++ Datastructures-Algorithms/src/main/java/com/example/datastructuresalgorithms/Datastructures/Linked/SingleLinked.java
@@ -2,7 +2,7 @@ package com.example.datastructuresalgorithms.Datastructures.Linked;
 
 import java.util.NoSuchElementException;
 
-public class SingleLinked<E> implements Linked<E>{
+public class SingleLinked<E> implements Linked<E> {
 
     private int size = 0;
 
@@ -20,7 +20,8 @@ public class SingleLinked<E> implements Linked<E>{
         }
     }
 
-    public SingleLinked() {}
+    public SingleLinked() {
+    }
 
     @Override
     public int size() {
@@ -53,13 +54,16 @@ public class SingleLinked<E> implements Linked<E>{
     public void add(int index, E e) {
         if (index == 0) {
             addFirst(e);
+            return;
         } else if (index == (size - 1)) {
             addLast(e);
+            return;
         } else if (index < 0 || index >= size) {
             throw new NoSuchElementException();
         }
         Node<E> x = head;
-        for (int i = 0; i < size; i++) {
+        for (int i = 1; i < size - 1; i++) {
+            x = x.next;
             if (i == index) {
                 Node<E> temp = x.next;
                 Node<E> newNode = new Node<>(e, null);
@@ -67,43 +71,133 @@ public class SingleLinked<E> implements Linked<E>{
                 newNode.next = temp;
                 break;
             }
-            x = x.next;
         }
         size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        if (head == null) throw new NoSuchElementException();
+        E element = head.element;
+        Node<E> next = head.next;
+        head.next = null;
+        head.element = null; // help gc
+        head = next;
+        size--;
+        return element;
     }
 
     @Override
     public E removeLast() {
-        return null;
+        if (tail == null) throw new NoSuchElementException();
+        E element = tail.element;
+        Node<E> prev = head;
+        for (int i = 1; i < size - 1; i++) {
+            prev = prev.next;
+        }
+        prev.next = null;
+        tail = prev;
+        size--;
+        return element;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == size - 1) {
+            return removeLast();
+        } else if (index < 0 || index >= size) {
+            throw new NoSuchElementException();
+        }
+        Node<E> x = head;
+        Node<E> y = head;
+        E element = null;
+        flag:
+        for (int i = 1; i < size - 1; i++) {
+            x = x.next;
+            if (index == i) {
+                for (int j = 0; j < size; j++) {
+                    if (j == i - 1) {
+                        y.next = x.next;
+                        element = x.element;
+                        x.next = null;
+                        x.element = null; // help gc
+                        break flag;
+                    }
+                    y = y.next;
+                }
+            }
+        }
+        size--;
+        return element;
     }
 
     @Override
     public E set(int index, E e) {
-        return null;
+        E oldValue = null;
+        if (index < 0 || index >= size) throw new NoSuchElementException();
+        Node<E> x = head;
+        for (int i = 0; i < size; i++) {
+            if (index == i) {
+                x.element = e;
+                break;
+            }
+            x = x.next;
+        }
+        return oldValue;
     }
 
     @Override
     public E getFirst() {
-        return null;
+        if (head == null) throw new NoSuchElementException();
+        return head.element;
     }
 
     @Override
     public E getLast() {
-        return null;
+        if (tail == null) throw new NoSuchElementException();
+        return tail.element;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index == 0) {
+            return getFirst();
+        } else if (index == size - 1) {
+            return getLast();
+        } else if (index < 0 || index >= size) {
+            throw new NoSuchElementException();
+        }
+        Node<E> x = head;
+        E element = null;
+        for (int i = 1; i < size - 1; i++) {
+            x = x.next;
+            if (index == i) {
+                element = x.element;
+            }
+        }
+        return element;
+    }
+
+    @Override
+    public String toString() {
+        Node<E> x = head;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append("[" + x.element + ",pointer] --> ");
+            x = x.next;
+        }
+        sb.delete(sb.length() - 5, sb.length() - 1);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        SingleLinked<String> singleLinked = new SingleLinked<>();
+        for (int i = 0; i < 9; i++) {
+            singleLinked.addLast(i + "");
+        }
+        System.out.println(singleLinked.get(6));
+        System.out.println(singleLinked);
     }
 }
